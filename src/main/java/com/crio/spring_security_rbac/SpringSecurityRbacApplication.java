ced16740421a01c55ecb10ed497349dac1822098
Sudhanshu Soni <sudhanshusoni500@gmail.com>
2026-03-29 22:33:59 +0530

    first commit

---
+++ src/main/java/com/crio/spring_security_rbac/SpringSecurityRbacApplication.java
@@ -0,0 +1,13 @@
+package com.crio.spring_security_rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crio/spring_security_rbac/SpringSecurityRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.spring_security_rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
