cfad50740c7a9829f782195d718f344c36517688
Sudhanshu Soni <sudhanshusoni500@gmail.com>
2026-03-30 01:21:27 +0530

    fix: change UserRepository ID type from String to Long

--- src/main/java/com/crio/rent_video/repository/UserRepository.java
+++ src/main/java/com/crio/rent_video/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.crio.rent_video.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
 
